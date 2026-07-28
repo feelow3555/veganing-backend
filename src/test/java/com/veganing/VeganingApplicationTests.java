d13824918ece832fb45ce8abefe2d13a2fac8180
feelow3555 <ghkdvlfgh7@hansung.ac.kr>
2026-07-28 13:20:40 +0900

    feat: initial project setup with domain package structure

---
+++ src/test/java/com/veganing/VeganingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veganing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeganingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
